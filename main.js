b02a6f2965184d12a572d36866c40c57c203d3a9
maracax <alaneraerts@live.be>
2020-10-15 22:36:13 +0200

    slide3

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let li = document.querySelectorAll('.leftBox ul li');
+let div = document.querySelectorAll('.contentBox');
+
+li.forEach(element => {
+    element.addEventListener('click' ,e=>{
+        e.target.style.borderRight='27px solid #89AD91';
+        li.forEach(element=>{
+            if(element!==e.target){
+                element.style.borderRight='27px solid rgba(255, 255, 255, .0)';
+            }
+        })
+        console.log(e.target);
+        let menuPos = e.target.getAttribute('data');
+        let query = document.querySelector(menuPos);
+        div.forEach(element=>{
+            if(element!==query){
+                element.style.display='none';
+            }
+        })
+        query.style.display='block';
+    })
+});
